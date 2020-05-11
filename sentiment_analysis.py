ce695519dc013ad4f45de1af56abb8e9bfae6b17
wesleyeduardocr7 <wesley.eduardo@acad.ifma.edu.br>
2020-05-11 09:30:50 -0300

    initialcommit

--- sentiment_analysis.py
+++ sentiment_analysis.py
@@ -3,15 +3,15 @@ import tweepy
 import numpy as np
 
 from pymongo import MongoClient
-client = MongoClient("mongodb+srv://wesleyeduardocr7:Wesd8123@cluster0-joqds.mongodb.net/test?retryWrites=true&w=majority")
+client = MongoClient("")
 #client = MongoClient('localhost',27017)
 
 db = client.get_database('sentiment_analysis')
 
-consumer_key = '8sqJYjU0Z3GSQjzY76Y3cEK5d'
-consumer_secret = 'ZzpEW2htXlxoY8pOOwmzl1irwAz1d6iguf4bh8YzBhwZEIOD6t'
-access_token = '1045828227015618560-U3JneyClKT3Hh2cLcSs9Psg60yoXGa'
-access_token_secret = 'u2Mw3urs5CVXnlv8VkSGDDx8IHZ0ByHTIfUeyrlrN87ZQ'
+consumer_key = ''
+consumer_secret = ''
+access_token = ''
+access_token_secret = ''
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
